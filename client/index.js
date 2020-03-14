77c3d89640769c540644658572fac9341376ace9
hoskra <hoskra@gmail.com>
2020-03-14 15:20:44 +0100

    quick gui

--- client/index.js
+++ client/index.js
@@ -3,17 +3,108 @@ import Example from './example'
 import Try1 from './try1'
 import Try2 from './try2'
 import Try3 from './try3'
+import Try4 from './try4'
 
-if (window.location.hash === '#start') {
+import Visualizer from './classes/visualizer'
 
-  // threejs try
-  // const try1 = new Try1()
+let artist, name;
+let artistElement, nameElement;
 
-  // interpolation and more stats try
-  const try2 = new Try2()
+let danceability;
+let energy;
+let key;
+let loudness;
+let mode;
+let speechiness;
+let acousticness;
+let instrumentalness;
+let liveness;
+let valence;
+let tempo;
+let duration_ms;
 
-  // const try3 = new Try3()
+let hiddenGui = false;
 
-} else {
-  auth()
+class Index extends Visualizer {
+  constructor () {
+    super({ volumeSmoothing: 100 })
+
+    if (window.location.hash === '#start') {
+
+      $('body').keyup(function(e){
+        if(e.keyCode == 32){
+            // user has pressed space
+            if (hiddenGui) {
+              hiddenGui = false;
+              $(".gui").removeClass("hiden");
+            } else {
+              hiddenGui = true;
+              $(".gui").addClass("hiden");
+            }
+        }
+     });
+
+      let app = new Try3();
+
+      $("#2").click(()=>{
+        app = undefined
+        app = new Try2()
+      })
+      $("#3").click(()=>{
+        app = undefined
+        app = new Try3()
+      })
+      $("#4").click(()=>{
+        app = undefined
+        app = new Try4()
+      })
+
+  } else {
+    auth()
+  }
+
 }
+hooks () {
+}
+
+paint ({ ctx, height, width, now }) {
+    artist = this.sync.state.currentlyPlaying.artists[0].name
+    name = this.sync.state.currentlyPlaying.name
+
+    $("#song-artist").html(artist)
+    $("#song-name").html(name)
+
+    danceability = this.sync.state.trackFeatures.danceability;
+    energy = this.sync.state.trackFeatures.energy;
+    key = this.sync.state.trackFeatures.key;
+    loudness = this.sync.state.trackFeatures.loudness;
+    mode = this.sync.state.trackFeatures.mode;
+    speechiness = this.sync.state.trackFeatures.speechiness;
+    acousticness = this.sync.state.trackFeatures.acousticness;
+    instrumentalness = this.sync.state.trackFeatures.instrumentalness;
+    liveness = this.sync.state.trackFeatures.liveness;
+    valence = this.sync.state.trackFeatures.valence;
+    tempo = this.sync.state.trackFeatures.tempo;
+    duration_ms = this.sync.state.trackFeatures.duration_ms;
+
+    $("#stats").html(
+      "<table>" +
+        "<tr><td>danceability</td><td>" + danceability + "</td></tr>" +
+        "<tr><td>energy</td><td>" + energy + "</td></tr>" +
+        "<tr><td>key</td><td>" + key + "</td></tr>" +
+        "<tr><td>loudness</td><td>" + loudness + "</td></tr>" +
+        "<tr><td>mode</td><td>" + mode + "</td></tr>" +
+        "<tr><td>speechiness</td><td>" + speechiness + "</td></tr>" +
+        "<tr><td>acousticness</td><td>" + acousticness + "</td></tr>" +
+        "<tr><td>instrumentalness</td><td>" + instrumentalness + "</td></tr>" +
+        "<tr><td>liveness</td><td>" + liveness + "</td></tr>" +
+        "<tr><td>valence</td><td>" + valence + "</td></tr>" +
+        "<tr><td>tempo</td><td>" + tempo + "</td></tr>" +
+        "<tr><td>duration_ms</td><td>" + duration_ms + "</td></tr>" +
+      "</table>"
+    )
+
+  }
+}
+
+let index = new Index()
---
+++ client/try4.js
@@ -0,0 +1,205 @@
+import Visualizer from './classes/visualizer'
+import * as THREE from 'three'
+
+import { makeGrids } from './fraviz/grid'
+
+var scene, camera, renderer;
+
+// DEFAULT VALUES
+let BACKGROUND_COLOR = 0x000000;
+
+var volumeMaterial = new THREE.MeshPhongMaterial( { color: 0xfc0303, side: THREE.DoubleSide } );
+var tatumMaterial = new THREE.MeshPhongMaterial( { color: 0xfcf403, side: THREE.DoubleSide } );
+var segmentMaterial = new THREE.MeshPhongMaterial( { color: 0x03fc0f, side: THREE.DoubleSide } );
+var beatMaterial = new THREE.MeshPhongMaterial( { color: 0x03fcf0, side: THREE.DoubleSide } );
+var barMaterial = new THREE.MeshPhongMaterial( { color: 0x0320fc, side: THREE.DoubleSide } );
+var sectionMaterial = new THREE.MeshPhongMaterial( { color: 0xfc03fc, side: THREE.DoubleSide } );
+
+let volumeObject;
+let tatumObject;
+let segmentObject;
+let beatObject;
+let barObject;
+let sectionObject;
+
+let volumeText;
+let tatumText;
+let segmentText;
+let beatText;
+let barText;
+let sectionText;
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+}
+
+// ( left-right, up-down, front-behind )
+// (x, y, z)
+
+function createObjects() {
+
+  const fontJson = require( "fonts/gentilis_regular.typeface.json" );
+  const font = new THREE.Font( fontJson );
+
+  var volumeGeo  = new THREE.TextGeometry( 'volume', { font: font,size: 55,height: 5,curveSegments: 12,bevelEnabled: true,bevelThickness: 10,bevelSize: 8,bevelOffset: 0,bevelSegments: 5} );
+  volumeGeo.computeBoundingBox();
+  volumeGeo.computeVertexNormals()
+  var tatumGeo  = new THREE.TextGeometry( 'tatum', { font: font,size: 55,height: 5,curveSegments: 12,bevelEnabled: true,bevelThickness: 10,bevelSize: 8,bevelOffset: 0,bevelSegments: 5} );
+  tatumGeo.computeBoundingBox();
+  tatumGeo.computeVertexNormals()
+  var segmentGeo  = new THREE.TextGeometry( 'segment', { font: font,size: 55,height: 5,curveSegments: 12,bevelEnabled: true,bevelThickness: 10,bevelSize: 8,bevelOffset: 0,bevelSegments: 5} );
+  segmentGeo.computeBoundingBox();
+  segmentGeo.computeVertexNormals()
+  var beatGeo  = new THREE.TextGeometry( 'beat', { font: font,size: 55,height: 5,curveSegments: 12,bevelEnabled: true,bevelThickness: 10,bevelSize: 8,bevelOffset: 0,bevelSegments: 5} );
+  beatGeo.computeBoundingBox();
+  beatGeo.computeVertexNormals()
+  var barGeo  = new THREE.TextGeometry( 'bar', { font: font,size: 55,height: 5,curveSegments: 12,bevelEnabled: true,bevelThickness: 10,bevelSize: 8,bevelOffset: 0,bevelSegments: 5} );
+  barGeo.computeBoundingBox();
+  barGeo.computeVertexNormals()
+  var sectionGeo  = new THREE.TextGeometry( 'section', { font: font,size: 55,height: 5,curveSegments: 12,bevelEnabled: true,bevelThickness: 10,bevelSize: 8,bevelOffset: 0,bevelSegments: 5} );
+  sectionGeo.computeBoundingBox();
+  sectionGeo.computeVertexNormals()
+
+  volumeObject = new THREE.Mesh( new THREE.SphereBufferGeometry( 10 ), volumeMaterial );
+  tatumObject = new THREE.Mesh( new THREE.SphereBufferGeometry( 10 ), tatumMaterial );
+  segmentObject = new THREE.Mesh( new THREE.SphereBufferGeometry( 10 ), segmentMaterial );
+  beatObject = new THREE.Mesh( new THREE.SphereBufferGeometry( 10 ), beatMaterial );
+  barObject = new THREE.Mesh( new THREE.SphereBufferGeometry( 10 ), barMaterial );
+  sectionObject = new THREE.Mesh( new THREE.SphereBufferGeometry( 10 ), sectionMaterial );
+
+  volumeText = new THREE.Mesh( new THREE.BufferGeometry().fromGeometry( volumeGeo ), volumeMaterial );
+  tatumText = new THREE.Mesh( new THREE.BufferGeometry().fromGeometry( tatumGeo ), tatumMaterial );
+  segmentText = new THREE.Mesh( new THREE.BufferGeometry().fromGeometry( segmentGeo ), segmentMaterial );
+  beatText = new THREE.Mesh( new THREE.BufferGeometry().fromGeometry( beatGeo ), beatMaterial );
+  barText = new THREE.Mesh( new THREE.BufferGeometry().fromGeometry( barGeo ), barMaterial );
+  sectionText = new THREE.Mesh( new THREE.BufferGeometry().fromGeometry( sectionGeo ), sectionMaterial );
+
+
+  scene.add(volumeObject)
+  scene.add(tatumObject)
+  scene.add(segmentObject)
+  scene.add(beatObject)
+  scene.add(barObject)
+  scene.add(sectionObject)
+
+  scene.add(volumeText)
+  scene.add(tatumText)
+  scene.add(segmentText)
+  scene.add(beatText)
+  scene.add(barText)
+  scene.add(sectionText)
+}
+
+export default class Try1 extends Visualizer {
+  constructor () {
+    super({ volumeSmoothing: 100 })
+
+    let canvas = document.getElementsByTagName('canvas')
+    for (let element of canvas) {
+      element.parentNode.removeChild(element);
+    }
+
+     // CAMERA
+     camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight , 1, 10000 );
+     camera.position.set( -100, 100, 1000 );
+     camera.lookAt( -100, 100, 0 );
+
+     // SCENE
+     scene = new THREE.Scene();
+     scene.background = new THREE.Color( BACKGROUND_COLOR );
+
+     // LIGHTS
+     var hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+     hemiLight.position.set( 0, 400, 0 );
+     scene.add( hemiLight );
+     var directionalLight = new THREE.DirectionalLight( 0xffffff );
+     directionalLight.position.set( 0, 200, 100 );
+     directionalLight.castShadow = true;
+     directionalLight.shadow.camera.top = 180;
+     directionalLight.shadow.camera.bottom = - 100;
+     directionalLight.shadow.camera.left = - 120;
+     directionalLight.shadow.camera.right = 120;
+     scene.add( directionalLight );
+
+     // RENDERER
+     renderer = new THREE.WebGLRenderer( { antialias: true } );
+     renderer.setPixelRatio( window.devicePixelRatio );
+     renderer.setSize( window.innerWidth, window.innerHeight );
+     renderer.shadowMap.enabled = true;
+
+     document.body.appendChild( renderer.domElement );
+     window.addEventListener( 'resize', onWindowResize, false );
+
+     makeGrids(scene, 2, 4700, 1);
+     createObjects();
+  }
+  hooks () {
+  }
+
+  getSync() {
+    return this.sync.state.currentlyPlaying.name + this.sync.state.currentlyPlaying.artists[0].name
+  }
+
+  paint ({ ctx, height, width, now }) {
+    renderer.render( scene, camera );
+
+    let speed = (this.sync.volume * 10) * (this.sync.volume * 10) * (this.sync.volume * 10)
+    speed = speed / 50
+
+    camera.position.z -= speed;
+
+    if(camera.position.z < -1000){
+      camera.position.z = 1000;
+    }
+
+    let camZ = camera.position.z;
+
+    let volume = this.sync.volume * 10;
+    let tatum = this.sync.tatum.progress * 10;
+    let segment = this.sync.segment.progress * 10;
+    let beat = this.sync.beat.progress * 10;
+    let bar = this.sync.bar.progress * 10;
+    let section = this.sync.section.progress * 10;
+
+    // console.log(this.sync.state.currentlyPlaying.name + this.sync.state.currentlyPlaying.artists[0].name)
+    // console.log(this.sync.section)
+
+    volumeObject.scale.set(volume ? volume : 0.00001 , volume ? volume : 0.00001 , volume ? volume : 0.00001 )
+    tatumObject.scale.set(tatum ? tatum : 0.00001, tatum ? tatum : 0.00001, tatum ? tatum : 0.00001)
+    segmentObject.scale.set(segment ? segment : 0.00001, segment ? segment : 0.00001, segment ? segment : 0.00001)
+    beatObject.scale.set(beat ? beat : 0.00001, beat ? beat : 0.00001, beat ? beat : 0.00001)
+    barObject.scale.set(bar ? bar : 0.00001, bar ? bar : 0.00001, bar ? bar : 0.00001)
+    sectionObject.scale.set(section ? section : 0.00001, section ? section : 0.00001, section ? section : 0.00001)
+
+    let x_position = -900;
+    let spacing = 300;
+    let y_position = -300;
+    let z_position = -1000 + camZ;
+
+    volumeObject.position.set(x_position, 0, z_position);
+    volumeText.position.set(x_position - 70, y_position, z_position);
+    x_position += spacing;
+
+    tatumObject.position.set(x_position, 0, z_position);
+    tatumText.position.set(x_position - 70, y_position, z_position);
+    x_position += spacing;
+
+    segmentObject.position.set(x_position, 0, z_position);
+    segmentText.position.set(x_position - 70, y_position, z_position);
+    x_position += spacing;
+
+    beatObject.position.set(x_position, 0, z_position);
+    beatText.position.set(x_position - 50, y_position, z_position);
+    x_position += spacing;
+
+    barObject.position.set(x_position, 0, z_position);
+    barText.position.set(x_position - 50, y_position, z_position);
+    x_position += spacing;
+
+    sectionObject.position.set(x_position, 0, z_position);
+    sectionText.position.set(x_position - 70, y_position, z_position);
+    x_position += spacing;
+  }
+}
